2dd99b8fbbcaacfa69a51a89fdd69497c1397385
Al Mamon <135543985+ALMAMON193@users.noreply.github.com>
2025-06-13 17:02:12 +0600

    Enhance admin authentication: refactor admin middleware to streamline logout process and enforce admin-only access; update dashboard panel to utilize admin middleware; modify root route to redirect to dashboard login.

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
+use App\Http\Middleware\Admin;
 use Filament\Support\Colors\Color;
 use Illuminate\Support\Facades\Hash;
 use Filament\Http\Middleware\Authenticate;
@@ -27,23 +28,7 @@ class DashboardPanelProvider extends PanelProvider
             ->default()
             ->id('dashboard')
             ->path('dashboard')
-            ->login(function () {
-                return \Filament\Pages\Auth\Login::make()
-                    ->authenticateUsing(function ($request) {
-                        $user = \App\Models\User::where('email', $request->email)->first();
-
-                        if (
-                            $user &&
-                            Hash::check($request->password, $user->password) &&
-                            $user->role === 'admin'
-                        ) {
-                            return $user;
-                        }
-
-                        return null;
-                    });
-            })
-
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -55,7 +40,6 @@ class DashboardPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -70,6 +54,7 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+                Admin::class,
             ]);
     }
 }
